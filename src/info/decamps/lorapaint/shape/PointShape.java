f9f0af7e51289179a79e8dfa9b0ee56feab8bd81
Regis Decamps <Regis Decamps@localhost>
2010-05-28 01:23:51 +0200

    when pressure increase, the circle is bigger and more transparent

--- src/info/decamps/lorapaint/shape/PointShape.java
+++ src/info/decamps/lorapaint/shape/PointShape.java
@@ -3,8 +3,6 @@ package info.decamps.lorapaint.shape;
 import info.decamps.lorapaint.LoraDrawable;
 import info.decamps.lorapaint.LoraSurfaceView;
 import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.Paint;
 import android.view.MotionEvent;
 
 public class PointShape extends LoraDrawable{
@@ -15,17 +13,14 @@ public class PointShape extends LoraDrawable{
 	boolean drawing=false;
 	private float x;
 	private float y;
-	private float pressure;
+	private float radius;
 
 	private static float MAX_RADIUS=30;
 	private static float MIN_RADIUS=8;
 
 	@Override
 	public void draw(Canvas canvas) {
-		Paint p = super.paint;
-		p.setAlpha((int)(255*pressure)/2+125);
-		float radius=MAX_RADIUS*pressure+MIN_RADIUS*(1f-pressure);
-		canvas.drawCircle(x, y, radius, p);
+		canvas.drawCircle(x, y, radius, paint);
 	}
 	
 	@Override
@@ -37,8 +32,13 @@ public class PointShape extends LoraDrawable{
 
 	    x = event.getX();
 	    y = event.getY();
-	    pressure=event.getPressure();
-		return false;
+	    // when pressure increase, the circle is bigger and more transparent
+	    float p=event.getPressure();
+	    radius=Math.max(radius,MAX_RADIUS*p+MIN_RADIUS*(1f-p));
+
+		paint.setAlpha(255-(int)(255*p)/2);
+		
+		return true;
 	}
 
 }
